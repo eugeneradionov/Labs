c120b48e8fdcdf049f36e2c2daa120d6ef331963
Eugene Radionov <eugene.radionov@gmail.com>
2015-06-24 19:28:44 +0300

    Try..Except added!

--- Ruby/Knapsack problem/Knapsack problem.rb
+++ Ruby/Knapsack problem/Knapsack problem.rb
@@ -1,24 +1,52 @@
-#Getting value and weight for each boxes
-p 'Please enter max weight of sack: '
-$maxweight = gets.chop.to_i
-p 'Please enter the nuber of items: '
-n = gets.chop.to_i
-if n == 0 or $maxweight == 0
-  p 'The number must be positive!'
-  exit
+#Getting max weight of sack, number of items, value and weight for each item.
+begin
+  p 'Please enter max weight of sack: '
+  $maxweight = gets.chop
+  Integer($maxweight)
+rescue
+  p 'The number must be positive integer!'
+  retry
 end
+$maxweight = $maxweight.to_i
+
+begin
+  p 'Please enter the number of items: '
+  n = gets.chop
+  Integer(n)
+rescue
+  p 'The number must be positive integer!'
+  retry
+end
+n = n.to_i
+
 $weight, $value = [], []
 i =0
 while i < n
-  p 'Please enter the weight of item: '
-  a = gets.chop.to_i
-  p 'Please enter the value of item: '
-  b = gets.chop.to_i
+  begin
+      p 'Please enter the weight of item: '
+      a = gets.chop
+      Integer(a)
+  rescue
+      p 'The number must be positive integer!'
+      retry
+  end
+  a = a.to_i
+
+  begin
+    p 'Please enter the value of item: '
+    b = gets.chop
+    Integer(b)
+  rescue
+    p 'The number must be positive integer!'
+    retry
+  end
+  b = b.to_i
+
   $weight << a
   $value << b
   i+=1
 end
-#p $value, $weight
+
 #Calculating optimal solution
 $all_weight, $all_value, i = 0, 0, 0
 $max_values, $max_weights =[], []
@@ -34,7 +62,7 @@ loop do
       $all_value -= $value[i-1]
     end
   else
+    print 'Optimal values= ', $all_value, "\n", 'Optimal weight= ', $all_weight
     break
   end
 end
-print 'Optimal values= ', $all_value, "\n", 'Optimal weight= ', $all_weight
